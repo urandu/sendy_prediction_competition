b8817587c3caa9180bef4aa65699c2a112e4e227
bildad namawa <bildadnamawa@gmail.com>
2019-10-30 07:45:15 +0300

    workin progress

--- clean_data.py
+++ clean_data.py
@@ -1,3 +1,13 @@
 import pandas as pd
 train_data = pd.read_csv('Train.csv')
 print(train_data.head()[:6])
+
+import matplotlib.pyplot as plt
+# %%matplotlib
+# %matplotlib inline
+
+fig, ax = plt.subplots()
+ax.scatter(train_data['Distance (KM)'], train_data['Time from Pickup to Arrival'])
+ax.set_title('Time from pick up to arrival')
+ax.set_xlabel('Distance KM')
+ax.set_ylabel('Time from pick up to arrival')
